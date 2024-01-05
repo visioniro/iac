065adafff42704d48bf87a750521aa8bfde7fd3b
Nir Abutbul <visionir@icloud.com>
2024-01-06 00:54:20 +0200

    a

--- IaC/images/webhook-api/api/src/crud.py
+++ IaC/images/webhook-api/api/src/crud.py
@@ -1,14 +1,14 @@
 from json import loads, dump, decoder
-
+from io import UnsupportedOperation
 def save_webhook(content: dict) -> None:
     with open('/data/webhook.json', 'w') as f:
         # f.seek(0)
         try:
-            data: list[dict] = loads(f.read())
+            data: list[dict] = loads(f)
             data.append(content)
             f.truncate()
             dump(data, f)
-        except decoder.JSONDecodeError:
+        except (decoder.JSONDecodeError, UnsupportedOperation):
             data = []
             data.append(content)
             dump(data, f)
